b9d3b81b712a906cbf36043b4ba182fd8700932e
AlexandreGazagnes <a_syoez@yahoo.fr>
2022-05-23 21:41:41 +0200

    clean code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cs188
 my css 188
+
+
+
+
+
+
+
+
+
+Changelog : 
+v0.0.0 : the brute force model with very simple db
--- src/main.py
+++ src/main.py
@@ -2,19 +2,19 @@ from src.modele import modele
 from src.utils import *
 
 
-all_towns = extract_all_town(modele)
-print(all_towns)
+def main():
+    """ """
 
+    all_towns = extract_all_town(modele)
+    # print(all_towns)
 
-all_strategies = cardinalize_various_depth_strategies(all_towns, [2, 3, 4, 5])
-print(all_strategies)
+    all_strategies = cardinalize_various_depth_strategies(all_towns, [2, 3, 4, 5])
+    # print(all_strategies)
 
+    valid_strategies = select_only_valid_strategies(
+        all_strategies,
+    )
+    print(valid_strategies)
 
-valid_strategies = select_only_valid_strategies(
-    all_strategies,
-)
-print(valid_strategies)
-
-
-best_strategies = find_best_strat(valid_strategies)
-print(best_strategies)
+    best_strategies = find_best_strat(valid_strategies)
+    print(best_strategies)
