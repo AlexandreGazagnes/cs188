186511f8b7798c5bbaacb6e880300889cd48388f
AlexandreGazagnes <a_syoez@yahoo.fr>
2022-05-23 21:29:43 +0200

    addd tests

---
+++ tests/test_main.py
@@ -0,0 +1,34 @@
+import pytest
+from src.main import *
+
+
+data_OK = [
+    ("rouen", "paris"),
+    ("rouen", "versailles"),
+    ("rouen", "lyon"),
+    ("rouen", "dieppe"),
+    ("paris", "versailles"),
+]
+
+
+data_KO = [
+    ("rouen", "lehavre"),
+    ("versailles", "versailles"),
+    ("versailles", "rouen"),
+]
+
+
+@pytest.mark.parametrize("dep,dest", data_OK)
+def test_main_ok(dep, dest):
+
+    tu = (dep, dest)
+    assert modelize(tu) > 0
+
+
+@pytest.mark.parametrize("dep,dest", data_KO)
+def test_main_ko(dep, dest):
+
+    li = [("nex_york", "paris")]
+
+    tu = (dep, dest)
+    assert modelize(tu) < 0
