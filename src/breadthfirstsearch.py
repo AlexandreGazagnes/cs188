ea2ea019ae0c06f683a7375c15804d4e659c90fd
AlexandreGazagnes <a_syoez@yahoo.fr>
2022-05-25 10:51:59 +0200

    ok

--- src/breadthfirstsearch.py
+++ src/breadthfirstsearch.py
@@ -18,7 +18,13 @@ class BreadthFirstSearch(TreeSearch):
 
         print("update_queue")
 
+        # reload the empty queue for the past level of depht
+        self.queued_strategies = [
+            i for i in self.evaluated_strategies if len(i) == self.current_depth
+        ]
+
         LI = []
+        # for each strat of the "old" queue find all dest
         for strat in self.queued_strategies:
             possible_dest = self.find_possible_dests(strat[-1])
             update = lambda i: (strat, [i])
@@ -31,17 +37,10 @@ class BreadthFirstSearch(TreeSearch):
     def extract_first_queue_to_active(self):
         """ """
 
-        pprint("-- extract_first_queue_to_active --")
-        pprint("BEFORE")
-        pprint(self.queued_strategies)
         self.active_strategy = self.queued_strategies[0]
-        pprint(self.active_strategy)
 
         if len(self.queued_strategies) >= 1:
             self.queued_strategies = self.queued_strategies[1:]
-
-        pprint("AFTER")
-        pprint(self.queued_strategies)
 
     def run(self):
         """ """
@@ -50,29 +49,22 @@ class BreadthFirstSearch(TreeSearch):
         self.current_depth = 1
         self.extract_first_queue_to_active()
         self.eval_strategy()
-
         if self.found * self.confirmed:
             raise ArithmeticError("solution found ")
-        li = [i for i in self.evaluated_strategies if len(i) == self.current_depth]
-        self.queued_strategies = li
+
         self.log()
 
-        # level 1
+        # level N
         while not (self.found * self.confirmed):
             self.update_queue()
             self.current_depth += 1
-            print(f"BEFORE LOOOP FOR self.self.current_depth  = {self.current_depth }")
-            print(f"BEFORE LOOOP FOR self.queued_strategies = {self.queued_strategies}")
-            for s in self.queued_strategies:
-                self.active_strategy = s
+
+            for strat in self.queued_strategies:
+                self.active_strategy = strat
                 self.eval_strategy()
 
-            li = [i for i in self.evaluated_strategies if len(i) == self.current_depth]
-            print(f"IN LOOP FOR evalueated srategies : {self.evaluated_strategies}")
-            print(f"IN LOOOP FOR li = {li}")
+                if self.found * self.confirmed:
+                    self.log()
+                    raise ArithmeticError("solution found ")
 
-            self.queued_strategies = li
             self.log()
-
-            if self.found * self.confirmed:
-                raise ArithmeticError("solution found ")
